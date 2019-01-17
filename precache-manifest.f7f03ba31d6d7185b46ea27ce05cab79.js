adb2ce3d202cee1e11d18f90bc46e260e5d1e66a
keeganosler <oslerkeegan@gmail.com>
2019-01-17 12:33:14 -0700

    Updates

--- precache-manifest.f7f03ba31d6d7185b46ea27ce05cab79.js
+++ precache-manifest.f7f03ba31d6d7185b46ea27ce05cab79.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Todo-List-App/static/js/runtime~main.d14d9656.js"
   },
   {
-    "revision": "7914cc18b2bb712d606e",
-    "url": "/Todo-List-App/static/js/main.7914cc18.chunk.js"
+    "revision": "5d9690855adc6c5b6813",
+    "url": "/Todo-List-App/static/js/main.5d969085.chunk.js"
   },
   {
     "revision": "2cfd82824bb64cef857d",
     "url": "/Todo-List-App/static/js/1.2cfd8282.chunk.js"
   },
   {
-    "revision": "7914cc18b2bb712d606e",
-    "url": "/Todo-List-App/static/css/main.5fbe712a.chunk.css"
+    "revision": "5d9690855adc6c5b6813",
+    "url": "/Todo-List-App/static/css/main.8bdec76f.chunk.css"
   },
   {
-    "revision": "ecd72850ffa6c0fabf58594a841d380f",
+    "revision": "50f11f3b45539c3c4e9aecc086c4d699",
     "url": "/Todo-List-App/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Todo-List-App/precache-manifest.0c9735c0ab2209c32f034c0ab8bf72b6.js"
+  "/Todo-List-App/precache-manifest.f7f03ba31d6d7185b46ea27ce05cab79.js"
 );
 
 workbox.clientsClaim();
